61ef11806e99e4f0b7e2d418f6f241194182dc9a
Antonio Delgado <tonydelro@gmail.com>
2025-07-08 12:46:05 -0600

    Synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { AuthModule } from './auth/auth.module';
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true, //
+      synchronize: false, //
     }),
     UsersModule,
     RolesModule,
