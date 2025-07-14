377a76d43a46d13738f8d7d1f34ce4794914d497
Antonio Delgado <tonydelro@gmail.com>
2025-07-13 22:08:42 -0600

    Remove company name from auth config response

--- src/system-settings/system-settings.controller.ts
+++ src/system-settings/system-settings.controller.ts
@@ -19,7 +19,6 @@ export class SystemSettingsController {
       color_primary: settings.color_primary,
       color_secondary: settings.color_secondary,
       color_tertiary: settings.color_tertiary,
-      company_name: settings.nombre,
     };
   }
 
